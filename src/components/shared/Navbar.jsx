35b12533f4a5deae88c6bf39083d28946acf1870
SSLiza <shajedasultanaliza2002@gmail.com>
2026-05-02 11:04:37 +0600

    Logical signin and signout button

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -4,10 +4,14 @@ import Link from 'next/link';
 import NavLink from './NavLink';
 import logo from '@/assets/logo.png';
 import { authClient } from '@/lib/auth-client';
+import { Avatar } from '@heroui/react';
 
 const Navbar = () => {
-  // const userInfo=authClient.useSession();
-  // console.log(userInfo.data?.user);
+  const userInfo = authClient.useSession();
+  console.log(userInfo.data?.user);
+  const handleSignOut = async () => {
+    await authClient.signOut();
+  }
   return (
     <div className="bg-[#7a756c] text-white border-b-0.5 sticky top-0 z-50">
       <div className="container mx-auto flex justify-between items-center px-4 py-3">
@@ -36,18 +40,33 @@ const Navbar = () => {
         {/* Right Side */}
         <div className="flex items-center gap-3">
 
-          {/* Login Button */}
-          <Link href="/login">
-            <button className="bg-[#1a1814] px-4 py-2 rounded-md btn font-medium">
-              Login
+          {!userInfo.data?.user && <ul>  {/* Login Button */}
+            <li><Link href="/login">
+              <button className="bg-[#1a1814] px-4 py-2 rounded-md btn font-medium">
+                Login
+              </button>
+            </Link></li>
+            <li><Link href="/register">
+              <button className="bg-[#1a1814] px-4 py-2 rounded-md btn font-medium">
+                Register
+              </button>
+            </Link> </li>
+          </ul>
+          }
+          {
+            userInfo.data?.user && 
+            <div className="flex items-center gap-3">
+              <Avatar>
+                <Avatar.Image src={userInfo.data.user.image} alt={userInfo.data.user.name} referrerPolicy="no-referrer" />
+                <Avatar.Fallback>{userInfo.data.user.name[0]}</Avatar.Fallback>
+              </Avatar>
+              <button onClick={handleSignOut}
+              className="bg-[#1a1814] px-4 py-2 rounded-md btn font-medium" 
+            >
+              Logout
             </button>
-          </Link>
-          <Link href="/register">
-            <button className="bg-[#1a1814] px-4 py-2 rounded-md btn font-medium">
-              Register
-            </button>
-          </Link>
-
+            </div>
+          }
         </div>
       </div>
     </div>
